4ad01e420c7ef0815658c5922636c6c7a70ff2f4
Shorya Malani <shoryamal@gmail.com>
2023-06-16 16:54:28 -0400

    fixing

--- api/User.py
+++ api/User.py
@@ -46,6 +46,7 @@ class User:
             cur_data['friends'] = [friend_id]
             dbs_worker.set_user_data(self.user_id, cur_data)
         self.user_data = dbs_worker.get_user_by_id(self.user_id)
+        self.get_friend_data()
         return True
     def get_friend_data(self):
         friend_data = []
